5b0af235acf898cf06a1e371cc00cd0793b64dfc
K G KRISHNAKANTH <95517599+krishna5557@users.noreply.github.com>
2023-04-18 23:29:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT=-e .
+
 
 def get_requirements()->List[str]:
     
@@ -15,7 +15,7 @@ def get_requirements()->List[str]:
         requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove('-e .')
     
     return requirements
           
